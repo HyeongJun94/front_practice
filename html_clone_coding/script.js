9c2e2f265c2fd5d6440e07fed3f11aae5d9add89
Hyeongjun94 <whow1101@naver.com>
2020-07-13 23:16:53 +0900

    Impelment 10% tutorial list

--- html_clone_coding/script.js
+++ html_clone_coding/script.js
@@ -1,6 +1,10 @@
-var slideIndex = 1;
-showSlides(slideIndex)
+const user_site = 1
+const project_site = 2
 
+var slideIndex = 1;
+
+showSlides(slideIndex)
+onSelected(user_site)
 
 // Next/previous controls
 function plusSlides(n) {
@@ -23,4 +27,25 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block"; 
 
   setTimeout(function() {showSlides(slideIndex+=1)},4000);
+}
+
+function onSelected(which){
+  var btn_user = document.getElementById("user-site-btn");
+  var btn_project = document.getElementById("project-site-btn");
+  
+  var doc_user = document.getElementById("user-site");
+  var doc_project = document.getElementById("project-site");
+
+  if(which == user_site){
+    btn_user.className = "selected"
+    btn_project.className = null
+    doc_user.style.display = 'block';
+    doc_project.style.display = 'none';
+  } else {
+    btn_project.className = "selected"
+    btn_user.className = null
+    doc_user.style.display = 'none';
+    doc_project.style.display = 'block';
+  }
+  
 }
