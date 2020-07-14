9517b1ad9df2fe9bc620c786128b5b1ce9e424a4
Hyeongjun94 <whow1101@naver.com>
2020-07-14 20:52:04 +0900

    Implement 70%

--- html_clone_coding/script.js
+++ html_clone_coding/script.js
@@ -1,10 +1,15 @@
 const user_site = 1
 const project_site = 2
 
+const a_terminal = 1
+const github_desktop = 2
+const i_dont_know = 3
+
 var slideIndex = 1;
 
 showSlides(slideIndex)
 onSelected(user_site)
+choiceList(a_terminal)
 
 // Next/previous controls
 function plusSlides(n) {
@@ -47,5 +52,49 @@ function onSelected(which){
     doc_user.style.display = 'none';
     doc_project.style.display = 'block';
   }
-  
+}
+
+
+function choiceList(which){
+  var btn_a_terminal = document.getElementById("a-terminal-btn")
+  var btn_github_desktop = document.getElementById("github-desktop-btn")
+  var btn_i_dont_know = document.getElementById("i-dont-know-btn")
+
+  var doc_a_terminal = document.getElementsByClassName("a-terminal")
+  var doc_github_desktop = document.getElementsByClassName("github-desktop")
+  var doc_i_dont_know = document.getElementsByClassName("i-dont-know")
+
+  btn_a_terminal.className = null;
+  btn_github_desktop.className = null;
+  btn_i_dont_know.className = null;
+
+  for(var i=0; i<doc_a_terminal.length; i++){
+    doc_a_terminal[i].style.display = 'none';
+  }
+  for(var i=0; i<doc_github_desktop.length; i++){
+    doc_github_desktop[i].style.display = 'none';
+  }
+  for(var i=0; i<doc_i_dont_know.length; i++){
+    doc_i_dont_know[i].style.display = 'none';
+  }
+
+  if(which == a_terminal){
+    btn_a_terminal.className = "selected";
+    for(var i=0; i<doc_a_terminal.length; i++){
+      doc_a_terminal[i].style.display = 'block';
+    }
+  }
+  else if(which == github_desktop){
+    btn_github_desktop.className = "selected";
+    for(var i=0; i<doc_github_desktop.length; i++){
+      doc_github_desktop[i].style.display = 'block';
+    }
+  }
+  else {
+    // which == i_dont_know
+    btn_i_dont_know.className = "selected";
+    for(var i=0; i<doc_i_dont_know.length; i++){
+      doc_i_dont_know[i].style.display = 'block';
+    }
+  }
 }
